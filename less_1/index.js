e529d3353b2a1867092d11bd7ec8c5af1c528c5e
LexaHood <allpetr@list.ru>
2023-05-10 21:28:33 +0300

    Выполнение части дз

--- less_1/index.js
+++ less_1/index.js
@@ -1,9 +1,19 @@
 const slides = document.querySelectorAll('.slide');
+const bg = document.querySelector('img');
 
 for (const slide of slides) {
   slide.addEventListener('click', () => {
+    const bg_new = slide.style.backgroundImage.slice(5, -2);
     clearActiveClasses();
     slide.classList.add('active');
+    bg.classList.add('img-change-animation');
+    setTimeout(() => {
+      bg.src = bg_new;
+      bg.classList.add('visible');
+    }, 200);
+    setTimeout(() => {
+      bg.classList.remove('img-change-animation', 'visible');
+    }, 400);
   });
 }
 
